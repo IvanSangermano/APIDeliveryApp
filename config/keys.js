5322df324e8a121c7847b21494ace0087aac8241
Ivan Sangermano <ivan.j.sanger@gmail.com>
2023-11-03 14:43:19 -0300

    Validation to LogIn with JWT

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secretOrKey: "GJlu$.[=g`wU1>t/CQ?a@Aa2ykM00}k^CP%t!Ai4^bB5UsQ8T}&Z+Q?%o_XJLM7"
+}
--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const server = http.createServer(app);
 const logger = require('morgan');
 const cors = require('cors')
 
+const passport = require('passport')
+
 //IMPORT ROUTES
 const usersRoute = require('./routes/userRoutes')
 
@@ -16,12 +18,16 @@ const usersRoute = require('./routes/userRoutes')
 const port = process.env.PORT || 3000
 
 //CONFIG APP
-app.use(logger('dev'))
-app.use(express.json());
 app.use(cors());
+app.use(passport.initialize());
+app.use(passport.session())
+require('./config/passport')(passport)
+
+app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
+app.use(logger('dev'))
 app.disable('x-powered-by')
 app.set('port', port)
 
@@ -30,7 +36,7 @@ usersRoute(app)
 
 
 //START SERVER
-server.listen(3000, () => {
+server.listen(3000, "localhost", () => {
     console.log('Servidor de Node.js en puerto ' + port + ' iniciada...')
 })
 
